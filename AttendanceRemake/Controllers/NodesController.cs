afd21835c31af609f8c502953a967d09b41bf461
Abdulrahman Alkandari <AYalkandari@pifss.gov.kw>
2025-09-02 13:48:11 +0300

    nodes

--- AttendanceRemake/Controllers/NodesController.cs
+++ AttendanceRemake/Controllers/NodesController.cs
@@ -1,6 +1,81 @@
-﻿namespace AttendanceRemake.Controllers
+﻿using AttendanceRemake.Models;
+using AttendanceRemake.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json.Nodes;
+
+namespace AttendanceRemake.Controllers
 {
-    public class NodesController
+    [ApiController]
+    [Route("[controller]")]
+    public class NodesController : ControllerBase
     {
+        private readonly ILogger<NodesController> _logger;
+        private IRepository _rep;
+        public NodesController(ILogger<NodesController> logger, IRepository rep)
+        {
+            _logger = logger;
+            _rep = rep;
+        }
+
+        [HttpGet]
+        [Route(nameof(GetNodeList))]
+        public async Task<ActionResult<List<Node>>> GetNodeList()
+        {
+            try
+            {
+                var nodes = await _rep.GetAsync<Node>();
+
+                return Ok(nodes);
+            }
+            catch (Exception ex)
+            {
+                return ResponseError(ex);
+            }
+
+        }
+        [HttpGet]
+        [Route(nameof(GetNodeBySerial))]
+        public async Task<ActionResult<Node>> GetNodeBySerial(string serial)
+        {
+            try
+            {
+                var nodes = await _rep.GetByIdAsync<Node>(serial);
+
+                return Ok(nodes);
+            }
+            catch (Exception ex)
+            {
+                return ResponseError(ex);
+            }
+        }
+        [HttpPost]
+        [Route(nameof(AddNewNode))]
+        public async Task<ActionResult<JsonObject>> AddNewNode(Node node)
+        {
+            try
+            {
+                await _rep.AddAsync<Node>(node);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return ResponseError(ex);
+            }
+        }
+
+        private ObjectResult ResponseError(Exception ex)
+        {
+            var stackTrace = new StackTrace(ex, true);
+            var frame = stackTrace.GetFrame(0);
+
+            var method = frame.GetMethod();
+
+            string methodName = method.Name; // Method name
+            string className = method.DeclaringType?.FullName; // Class name
+
+            return StatusCode(500, $"Error: {ex.Message}\nException Type: {ex.GetType()}\nLocation: {className}");
+
+        }
     }
 }
